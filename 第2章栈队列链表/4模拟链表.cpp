9cef799c0d1f6a6e365a7621c63c97cc68162bf3
webturing <zj@webturing.com>
2020-08-29 10:50:47 +0800

    第二章数据结构

---
+++ 第2章栈队列链表/4模拟链表.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+using ll=long long;
+#define endl '\n'
+#define FOR(i, x, y) for (decay<decltype(y)>::type i = (x), _##i = (y); i < _##i; ++i)
+#define FORD(i, x, y) for (decay<decltype(x)>::type i = (x), _##i = (y); i > _##i; --i)
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    //TODO
+
+    return 0;
+}
